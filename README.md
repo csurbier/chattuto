4c78bf128700eadbad6a87bb340d891a529ed9f8
Christophe <csurbier@idevotion.fr>
2021-03-07 09:32:41 +0100

    day one source code

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # chattuto
+
+Source code repository.
+
+These tutorials will focus on how to create a real world mobile chat application with Ionic/Angular, Capacitor and Django. The **Chat** application will manage private chat between two persons and not a group chat application. 
+ 
+
+For this application, we can list these main functionalities :
+
+* Login / Register / Forget password screens
+* List of chats screen
+* Chat screen
+
+So we will learn each steps required to create a real world chat mobile application with it's associated backend in a day to day process.
+
+On backend, we will use Django and Django channels to have a socket based chat and we will also learn how to save and retrieve chat history. 
+
+On frontend, we will develop a Ionic application with angular framework, which will communicate with our backend to manage the chats. we will deploy the application on mobile devices using Capacitor. 
---
+++ day-one/ChatTuto/Backend/chattuto/chat/models.py
@@ -0,0 +1,93 @@
+# *coding: utf-8*
+from __future__ import unicode_literals
+
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.gis.db import models
+import uuid
+from django.utils.translation import ugettext_lazy as _
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('active'), default=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    lastConnexionDate = models.DateTimeField(null=True, blank=True)
+    valid = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    @property
+    def last_login(self):
+        return self.lastConnexionDate
+
+    def __str__(self):
+        return u'%s' % (self.email)
+
+class Chat(models.Model):
+    fromUser = models.ForeignKey(User, db_index=True,on_delete=models.SET_NULL, null=True,related_name="fromuser")
+    toUser = models.ForeignKey(User, db_index=True,on_delete=models.SET_NULL, null=True,related_name="toUser")
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    unique_together = (("fromUser", "toUser"),)
+
+    def __str__(self):
+        return u'%s - %s' % (self.fromUser,self.toUser)
+
+
+class Message(models.Model):
+    refChat = models.ForeignKey(Chat, db_index=True,on_delete=models.CASCADE)
+    message = models.TextField()
+    msg_type = (
+        (0, "TEXT"),
+        (1, "GEOLOC"),
+        (2, "PHOTO"),
+    )
+    type = models.IntegerField(choices=msg_type, default=0)
+    extraData = models.CharField(default='', null=True, blank=True, max_length=255)
+    author = models.ForeignKey(User, db_index=True,related_name='author',on_delete=models.SET_NULL,null=True)
+    isRead = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return u'%s - %d' % (self.refChat,self.type)

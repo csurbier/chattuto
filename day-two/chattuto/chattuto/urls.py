8bb4e80a07b9847d64c7d63e1effe863485361b5
Christophe <csurbier@idevotion.fr>
2021-03-17 09:26:08 +0100

    day two tutorial

---
+++ day-two/chattuto/chattuto/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include
+from django.urls import path
+from django.contrib import admin
+
+urlpatterns = [
+    path('chat/', include('chat.urls')),
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+]
